076801bdab5efd9a604f993e69e704d7a3333863
Aladin Zorgui <zorguiala1@gmail.com>
2024-11-21 19:10:21 +0100

    Update Workflow

--- eslint.config.js
+++ eslint.config.js
@@ -1,28 +1,33 @@
-import js from '@eslint/js';
-import globals from 'globals';
-import reactHooks from 'eslint-plugin-react-hooks';
-import reactRefresh from 'eslint-plugin-react-refresh';
-import tseslint from 'typescript-eslint';
+import js from "@eslint/js";
+import globals from "globals";
+import reactHooks from "eslint-plugin-react-hooks";
+import reactRefresh from "eslint-plugin-react-refresh";
+import eslintPluginTS from "@typescript-eslint/eslint-plugin";
+import tsParser from "@typescript-eslint/parser";
 
-export default tseslint.config(
-  { ignores: ['dist'] },
+export default [
   {
-    extends: [js.configs.recommended, ...tseslint.configs.recommended],
-    files: ['**/*.{ts,tsx}'],
+    ignores: ["dist"], // Files to ignore
+  },
+  {
+    files: ["**/*.{ts,tsx}"], // Target TypeScript files
     languageOptions: {
-      ecmaVersion: 2020,
-      globals: globals.browser,
+      ecmaVersion: 2020, // ECMAScript version
+      globals: globals.browser, // Browser globals
+      parser: tsParser, // TypeScript parser
     },
     plugins: {
-      'react-hooks': reactHooks,
-      'react-refresh': reactRefresh,
+      "react-hooks": reactHooks,
+      "react-refresh": reactRefresh,
+      "@typescript-eslint": eslintPluginTS,
     },
     rules: {
-      ...reactHooks.configs.recommended.rules,
-      'react-refresh/only-export-components': [
-        'warn',
+      ...reactHooks.configs.recommended.rules, // React hooks rules
+      ...eslintPluginTS.configs.recommended.rules, // TypeScript rules
+      "react-refresh/only-export-components": [
+        "warn",
         { allowConstantExport: true },
       ],
     },
-  }
-);
+  },
+];
